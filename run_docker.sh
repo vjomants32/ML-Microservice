c99034f0af44dc97b4a2d14d5ae221acce7122d4
Valdis Jomants <61548467+vjomants32@users.noreply.github.com>
2020-05-23 03:12:06 +0300

    Commit 1.0

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build . --tag=valdisudemy
+
+
+# Step 2:
+# List docker images
+docker image ls
+
+# Step 3:
+# Run flask app
+docker run -it --rm -p 8000:80 valdisudemy
